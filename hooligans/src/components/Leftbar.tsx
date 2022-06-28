44e176ba63e40e358436e7abacab024039c5137a
npunjani3 <npunjani404@gmail.com>
2022-06-28 19:04:09 -0400

    Nested Lists Added to Left Nav

--- hooligans/src/components/Leftbar.tsx
+++ hooligans/src/components/Leftbar.tsx
@@ -1,4 +1,5 @@
-import {Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import { useState } from 'react';
+import {Box, Collapse, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
 import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 import SportsBaseballIcon from '@mui/icons-material/SportsBaseball';
 import SportsScoreIcon from '@mui/icons-material/SportsScore';
@@ -6,30 +7,54 @@ import SportsVolleyballIcon from '@mui/icons-material/SportsVolleyball';
 
 
 export const Leftbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+      };
+
     return (
             <Drawer sx={{width: '240px', backgroundColor: '#1976d2', flexShrink: 0,
             [`& .MuiDrawer-paper`]: { color: '#fff',width: '240px', height: '100vh', boxSizing: 'border-box', backgroundColor: '#1976d2', position: 'relative' },}} variant="permanent" anchor="left">
                 <Box sx={{ overflow: 'auto' }}>
                     <Divider />
-                        <List>
-                            <ListItem key= 'Soccer' >
-                                <ListItemButton>
+                        <List component="nav">
+                            <ListItem >
+                                <ListItemButton onClick={handleClick}>
                                     <ListItemIcon>
                                         <SportsSoccerIcon sx={{color: '#fff'}}/>
                                     </ListItemIcon>
                                     <ListItemText primary='Soccer' />
-                                </ListItemButton>                               
+                                </ListItemButton>                  
                             </ListItem>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <List component = "div" disablePadding>
+                                    <ListItemButton sx={{ pl: 4 }}>
+                                        <ListItem >
+                                            <ListItemText primary="UEFA Champions League" />
+                                        </ListItem>
+                                    </ListItemButton>
+                                </List>
+                            </Collapse>    
                             <ListItem >
-                                <ListItemButton>
+                                <ListItemButton onClick={handleClick}>
                                     <ListItemIcon>
                                         <SportsBaseballIcon sx={{color: '#fff'}}/>
                                     </ListItemIcon>
                                     <ListItemText primary='Baseball'/>
                                 </ListItemButton>
                             </ListItem>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <List component = "div" disablePadding>
+                                    <ListItemButton sx={{ pl: 4 }}>
+                                        <ListItem >
+                                            <ListItemText primary="MLB" />
+                                        </ListItem>
+                                    </ListItemButton>
+                                </List>
+                            </Collapse>    
                             <ListItem >
-                                <ListItemButton>
+                                <ListItemButton id='f1-btn'>
                                     <ListItemIcon>
                                         <SportsScoreIcon sx={{color: '#fff'}}/>
                                     </ListItemIcon>
@@ -37,7 +62,7 @@ export const Leftbar = () => {
                                 </ListItemButton>
                             </ListItem>
                             <ListItem >
-                                <ListItemButton>
+                                <ListItemButton id='volleyball-btn'>
                                     <ListItemIcon>
                                         <SportsVolleyballIcon sx={{color: '#fff'}}/>
                                     </ListItemIcon>
